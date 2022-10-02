15ab3f05b30e7756c7ff66e10ac5d8dc167c1df1
luisiJavo <luis.aranda.lillo@gmail.com>
2022-10-02 23:47:08 +0200

    env settings

--- speec_recognition_core/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpeecRecognitionCoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'speec_recognition_core'
--- speechRecognition/wsgi.py
+++ speechRecognition/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'speechRecognition.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speechRecognition.settings")
 
 application = get_wsgi_application()
